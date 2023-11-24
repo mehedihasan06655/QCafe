65af7bccfd5afff6d333140fdf55b4bc1ad9be9b
mehedi-hasan <mh6040500@gmail.com>
2023-11-24 19:05:09 +0600

    menu half done

--- main.js
+++ main.js
@@ -10,3 +10,12 @@ var swiper = new Swiper(".mySwiper", {
       clickable: true,
     },
   });
+
+  // Menu open close
+  let menu = document.querySelector(".menu-icon");
+  let navbar = document.querySelector(".navbar");
+  
+  menu.onclick =()=>{
+    menu.classList.toggle('move');
+    navbar.classList.toggle('open-menu');
+  }
